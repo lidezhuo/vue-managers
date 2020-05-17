5bca8bc6e95358e0f80e3e1152029dfdb4eb603f
lidezhuo <1713876326@qq.com>
2020-05-18 00:49:37 +0800

    完成了登录模块

---
+++ src/src/plugins/element.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import { Form, FormItem, Input, Button, Message, Container, Header, Aside, Main, Submenu, MenuItem, Menu, MenuItemGroup, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Select, Option, Tag, Tree, Cascader } from 'element-ui'
+
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Menu)
+Vue.use(MenuItemGroup)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Cascader)
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
